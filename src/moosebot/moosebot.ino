4108f40a737827813b8d1152071a6b6745ca226e
Seamus Cawley <seamus_cawley@rapid7.com>
2019-02-06 16:37:16 +0000

    basic ball counting function

---
+++ src/moosebot/moosebot.ino
@@ -0,0 +1,37 @@
+int ball_counter_pin = 3; // define the obstacle avoidance sensor interface
+int val;// define numeric variables val
+
+int NUM_BALLS_COLLECTED = 0;
+
+bool ball_passing = true;
+
+void setup ()
+{
+  Serial.begin(9600);
+  pinMode (ball_counter_pin, INPUT);
+}
+void loop ()
+{
+  count_balls();
+}
+
+
+void count_balls()
+{
+  val = digitalRead (ball_counter_pin);
+  if (val == HIGH )
+  {
+    if (!ball_passing){
+      ball_passing = true;
+      NUM_BALLS_COLLECTED ++;
+      Serial.print("Num balls collected so far:");
+      Serial.println(NUM_BALLS_COLLECTED);
+      delay(50);
+    }
+  }
+  else
+  {
+    delay(50);
+    ball_passing = false;
+  }
+}
